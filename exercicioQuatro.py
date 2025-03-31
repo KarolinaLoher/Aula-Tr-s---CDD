68f1e97222a81e3b797e231241c27fe79c37c1ec
Karolina Loher <karolinavitoria909@gmail.com>
2025-03-31 09:49:23 -0300

    Exercicio quatro.py feito

---
+++ exercicioQuatro.py
@@ -0,0 +1,12 @@
+numeroUm = float(input("Digite um número: "))
+numeroDois = float(input("Digite outro número: "))
+
+soma = numeroUm + numeroDois
+subtracao = numeroUm - numeroDois
+multiplicacao = numeroUm * numeroDois
+divisao = numeroUm / numeroDois
+
+print(f" Sua soma do {numeroUm} mais o {numeroDois} é {soma};\n "
+      f"Sua subtração do {numeroUm} menos o {numeroDois} é {subtracao}\n"
+      f" Sua multiplicação do {numeroUm} vezes o {numeroDois} é {multiplicacao}\n "
+      f"Sua divisão do {numeroUm} dividido por {numeroDois} é {divisao}")
