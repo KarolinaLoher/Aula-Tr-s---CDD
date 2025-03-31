33e72f1b22c81623e07dabb2ce38fdb8cc4a556a
Karolina Loher <karolinavitoria909@gmail.com>
2025-03-31 11:02:19 -0300

    Exercício seis.py

---
+++ exercicioSeis.py
@@ -0,0 +1,9 @@
+from selectors import SelectSelector
+
+numeroUm = int(input("Digite o primeiro número: "))
+numeroDois = int(input("Digite o segundo número: "))
+
+if numeroUm>numeroDois :
+    print(numeroDois,numeroUm)
+else:
+    print(numeroUm,numeroDois)
